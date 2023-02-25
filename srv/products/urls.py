e68fed5a192a9e4b78b465f5ecda2b2088adbe45
malikovboo <malikovboo@gmail.com>
2023-02-25 20:45:38 +0600

    added /

--- srv/products/urls.py
+++ srv/products/urls.py
@@ -5,13 +5,13 @@ from . import views
 
 urlpatterns = [
     path('/', item_list, name='item_list'),
-    path('/item/<int:pk>', item_page, name='item_page'),
-    path('/item/<int:pk>/add_to_order', add_item_to_order, name='add_item_to_order'),
-    path('/success/', success, name='success'),
-    path('/cancel/', cancel, name='cancel'),
-    path('/buy/<int:pk>', buy, name='buy'),
-    path('/order_list', order_list, name='order_list'),
-    path('/buy_order', buy_order, name='buy_order'),
-    path('/clear_order', clear_order, name='clear_order'),
+    path('item/<int:pk>', item_page, name='item_page'),
+    path('item/<int:pk>/add_to_order', add_item_to_order, name='add_item_to_order'),
+    path('success/', success, name='success'),
+    path('cancel/', cancel, name='cancel'),
+    path('buy/<int:pk>', buy, name='buy'),
+    path('order_list', order_list, name='order_list'),
+    path('buy_order', buy_order, name='buy_order'),
+    path('clear_order', clear_order, name='clear_order'),
 ]
 
